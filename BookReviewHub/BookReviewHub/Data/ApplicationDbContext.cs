276d2118921ba5f4c012b6202bc873767acc3366
Stanislav Stoychev <stanislavstoychev98@gmail.com>
2026-02-22 03:01:43 +0200

    Database migration

---
+++ BookReviewHub/BookReviewHub/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookReviewHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookReviewHub.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
